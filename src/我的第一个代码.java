409cbe20528aab0913ca57d40c3925dea195defd
764642764 <764642764@qq.com>
2020-07-18 14:23:03 +0800

    我的第一个代码

--- src/我的第一个代码.java
+++ src/我的第一个代码.java
@@ -1,6 +1,19 @@
+import java.time.*;
+import java.util.Date;
+
 public class 我的第一个代码 {
     public static void main(String [] args)
     {
-        System.out.println("小傻瓜");
+        /*String greeting="help";
+        String s=greeting.substring(0,3);
+        System.out.println(s);
+        String all=String.join("/","S","M","l");
+        System.out.println(all);
+        String b=greeting.substring(0,3)+"123";
+        System.out.println(b);*/
+        //System.out.printf("%1$s %2$tB %2$te,%2$tY","Due date:" ,new Date());
+        String[] arges = {"1,2,3","ssd","ssd"};
+        for (String elements : arges)
+        System.out.println(elements);
     }
 }
